0f84c205cfd9577d4e35f5682e080887d2b9dff1
KovD3v <tommaso.coviello2@gmail.com>
2024-09-25 20:25:42 +0200

    grid

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -163,7 +163,7 @@ export default function Shop() {
 								className="w-64"
 							/>
 						</CardContent>
-						<CardFooter className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center">
+						<CardFooter className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-items-center">
 							{category.subCategories?.map((subCategory) => (
 								<Card
 									key={subCategory.title}
